cd407898df2b05fa6d6349467c784c488ba767ef
Alex <jvaughn1230@gmail.com>
2023-12-25 09:57:55 -0800

    added undrline & card flip animations

--- src/components/Animations/Animations.jsx
+++ src/components/Animations/Animations.jsx
@@ -7,10 +7,26 @@ const Animations = () => {
     triggerOnce: true,
   });
 
+  const { ref: cardsViewRef, inView: cardsVisible } = useInView({
+    triggerOnce: true,
+  });
+
   return (
     <div className="full">
       {/* Top Section */}
-      <div className="section1">Section 1</div>
+      <div className="section1">
+        <nav className="test-nav">
+          <a href="/" className="hover-underline-animation">
+            Home
+          </a>
+          <a href="/" className="hover-underline-animation">
+            Home
+          </a>
+          <a href="/" className="hover-underline-animation">
+            Home
+          </a>
+        </nav>
+      </div>
 
       {/* Wave section */}
       <div className="section2">
@@ -33,6 +49,24 @@ const Animations = () => {
           Cover
         </div>
         Section 3
+      </div>
+
+      <div className="section5">
+        <h1>Section 5</h1>
+        <div className="testcard-container" ref={cardsViewRef}>
+          <div className={`test-card1 ${cardsVisible ? "showcard" : ""} `}>
+            {" "}
+          </div>
+          <div
+            className={`test-card2 ${cardsVisible ? "showcard" : ""} `}
+          ></div>
+          <div className="pullup-container">
+            Pull up
+            <div
+              className={`hide2 ${cardsVisible ? "reveal-drop" : ""} `}
+            ></div>
+          </div>
+        </div>
       </div>
     </div>
   );
