686795707e77528541da8196ac6d59d475ed576d
Alex <jvaughn1230@gmail.com>
2023-12-27 15:48:19 -0800

    added animation to navbar

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ import Skills from "./Sections/Skills/Skills";
 import Contact from "./Sections/Contact/Contact";
 import Projects from "./Sections/Projects/Projects";
 import Footer from "./components/Footer/Footer";
-import Wave3 from "./components/wave3/Wave3";
-import Animations from "./components/Animations/Animations";
 
 function App() {
   return (
     <div className="App">
-      <Animations />
-      {/* <Hero />
+      <Hero />
       <About />
       <Skills />
       <Projects />
       <Contact />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
