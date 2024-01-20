e95a1e2767e4793d69ec8d38bd80ba5004bb101f
Alex <jvaughn1230@gmail.com>
2024-01-19 21:58:17 -0800

    adding animation

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import { useInView } from "react-intersection-observer";
 import "./ProjectCard.css";
 import Modal from "../Modal/Modal";
 
 const ProjectCard = ({ project }) => {
   const [hovered, setHovered] = useState(false);
   const [openModal, setOpenModal] = useState(false);
+
+  const { ref: cardViewRef, inView: cardVisible } = useInView({
+    triggerOnce: true,
+  });
 
   const closeModal = () => setOpenModal(false);
   console.log(project.name + " " + openModal);
@@ -15,9 +20,10 @@ const ProjectCard = ({ project }) => {
 
   return (
     <div
-      className="project-card"
+      className={`project-card ${cardVisible ? "show-project-card" : ""}`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
+      ref={cardViewRef}
     >
       <img src={project.cover} alt="dinobeach" />
       <div className={`${hovered ? "portfolio-box" : "hide"}`}>
