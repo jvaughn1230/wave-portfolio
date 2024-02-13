27d32be9286eb27a56b38b6d7a1d4a60d77bdb3d
Alex <jvaughn1230@gmail.com>
2024-02-12 21:43:22 -0800

    fixed curved container & started curve on pull away

--- src/Sections/Contact/Contact.js
+++ src/Sections/Contact/Contact.js
@@ -3,6 +3,9 @@ import { useInView } from "react-intersection-observer";
 import Mailer from "../../components/Mailer/Mailer";
 import "./Contact.css";
 
+// Temp
+import CurvedContainer from "../../components/CurvedContainer/CurvedContainer";
+
 const Contact = () => {
   const { ref: viewRef, inView: sectionVisible } = useInView({
     triggerOnce: true,
@@ -11,11 +14,13 @@ const Contact = () => {
   console.log(sectionVisible);
 
   return (
-    <div id="contact" className="section contact-section" ref={viewRef}>
-      <div className={`${sectionVisible ? "remove-cover" : ""} cover`}></div>
-      <h2 className="section-header">Contact</h2>
-      <Mailer />
-    </div>
+    <CurvedContainer>
+      <div id="contact" className="section contact-section" ref={viewRef}>
+        <div className={`${sectionVisible ? "remove-cover" : ""} cover`}></div>
+        <h2 className="section-header">Contact</h2>
+        <Mailer />
+      </div>
+    </CurvedContainer>
   );
 };
 
