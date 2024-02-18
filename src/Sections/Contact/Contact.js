6c67ef0f94d62d45f2b5984200dec940a7cecfdb
Alex <jvaughn1230@gmail.com>
2024-02-18 15:31:28 -0800

    added fish options

--- src/Sections/Contact/Contact.js
+++ src/Sections/Contact/Contact.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Mailer from "../../components/Mailer/Mailer";
 import "./Contact.css";
-
-// Temp
-// import CurvedContainer from "../../components/CurvedContainer/CurvedContainer";
+import { GiTropicalFish } from "react-icons/gi";
 
 const Contact = () => {
   return (
-    <div>
-      <div id="contact" className="section contact-section">
-        {/* <div className={`${sectionVisible ? "remove-cover" : ""} cover`}></div> */}
-        <h2 className="section-header">Contact</h2>
-        <Mailer />
-      </div>
+    <div id="contact" className="section contact-section">
+      {/* <GiTropicalFish className="contact-fish contact-fish-1" />
+      <GiTropicalFish className="contact-fish contact-fish-2" />
+      <GiTropicalFish className="contact-fish contact-fish-3" /> */}
+
+      <h2 className="section-header">Contact</h2>
+      <Mailer />
     </div>
   );
 };
